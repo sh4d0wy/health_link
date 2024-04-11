19eb81b807b2c5a2da1f0af716d362ef014f87d7
sh4d0wy <sakshambhugra8@gmail.com>
2024-04-11 21:48:55 +0530

    Added context

--- frontend/app/community/chats/page.jsx
+++ frontend/app/community/chats/page.jsx
@@ -1,27 +1,27 @@
 "use client"
 import io from 'socket.io-client'
-import React,{useState,useEffect} from 'react'
+import React,{useState,useEffect, useContext} from 'react'
+import UserContext from '@/app/_Context/UserContext';
 
 const page = () => {
     const [socket, setSocket] = useState(null);
     const [message, setMessage] = useState('');
     const [receivedMsg,setReceivedMsg] = useState([])
-
+    const {user} = useContext(UserContext);
+    
     useEffect(() => {
         if (!socket) {
-            // Create a Socket.IO client instance, connecting to the Node.js backend
-            const newSocket = io('http://localhost:3001'); // Replace with your backend's URL
+            const newSocket = io('http://localhost:3001'); 
             setSocket(newSocket);
         }
         if(socket){
 
             socket.on('user-message',(data)=>{
                 setReceivedMsg((prevMsg)=>[...prevMsg,data]);
-                // console.log(typeof(data));
                 console.log("received message", data);
             })
         }
-        return () => { // Clean up socket connection on unmount
+        return () => { 
             if (socket) socket.disconnect();
         };
     }, [socket]);
@@ -40,19 +40,21 @@ const page = () => {
     };
 
     return (
-        <div className='w-full h-[100vh] flex flex-col bg-blue-500 gap-3  items-center justify-center'>
-            <div className='w-[80vw] h-[80vh] rounded-3xl flex flex-col p-10 bg-white gap-10  justify-end'>
+        <>
+        <div className='w-full h-[100vh] flex flex-col bg-blue-500 gap-6  items-center justify-center'>
+            <div className='text-4xl font-bold'>Community Chat</div>
+            <div className='w-[80vw] overflow-y-auto scrollable h-[80vh] rounded-3xl flex flex-col p-16 bg-white gap-10  justify-end'>
                 <div className='flex flex-col gap-4' >
             {receivedMsg.length>0&&
             receivedMsg.map((msg,index)=>{
                 return(
-                    <div className='bg-blue-100 rounded-xl flex flex-col h-full text-black max-w-1/2 px-4 py-2' >
+                    <div className='bg-blue-100 rounded-xl flex flex-col h-full text-black w-1/4 px-4 py-2' >
                     <div>
-                    <sup>Username</sup>
+                    <sup>{user.userName}</sup>
                     </div>
                     <div className='flex justify-between'>
                     <p className='text-black' key={index}>{msg}</p>
-                    <sub>13:14</sub>
+                    <sub>01:14</sub>
                     </div>
                     </div>
                 )
@@ -60,12 +62,18 @@ const page = () => {
                 
             </div>
             <div className='flex gap-10 w-full justify-center'>
-            <input className='text-black px-4 py-2 rounded-xl bg-gray-200 w-3/4  h-12 ' type="text" value={message} onChange={handleChangeMessage} />
+            <input className='text-black px-4 py-2 rounded-xl bg-neutral-200 w-3/4  h-12 outline-none' placeholder='Enter your message' type="text" value={message} onKeyDown={(e)=>{
+                if(e.key==="Enter"){
+                    handleSendMessage();
+                }
+            }}
+            onChange={handleChangeMessage} />
             <button className='bg-blue-200 rounded-xl px-4 py-2 text-black w-1/4'onClick={handleSendMessage}>Send Message</button>
             </div>
             
         </div>
         </div>
+        </>
     );
 };
 
